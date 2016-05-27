936cc92f48eef49365b67de6d9cec94c4d9ec098
SkingXQ <skingxq@gmail.com>
2016-05-27 14:05:21 +0800

    version for project

---
+++ gui/version.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# coding=utf8
+from __future__ import unicode_literals
+from youtube_dl.version import __version__ as ydlv
+
+__version__ = "1.0"
+
+__ydlv__ = "2015.12.3.3" #ydlv
+
+__help_info__ = """<h3>About Youtube DL</h3>
+
+<p>GUI for <a href="https://github.com/rg3/youtube-dl" >Youtube DL.</a></p>
+<br />
+<p>Author : skingxq</p>
+<p>Youtube DL version: %s</p>
+<p>Gui version: %s</p>
+"""
